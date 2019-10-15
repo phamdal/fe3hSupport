39bb201c977a12ffbecd2b73439951f8f635928b
Dalena <dalenapham35@gmail.com>
2019-10-14 22:31:06 -0700

    Adding Missing Lecture Answers

---
+++ lectureAnswers.js
@@ -0,0 +1,253 @@
+var lectureAnswers = 
+[
+    {
+        question: "You know how sometimes an ordinary girl can be more popular than an exceptional beauty? Or how, with aristocrats, some prefer a viscount when they could have a duke instead? Why do you think that is?", 
+        answer: "Fancy meals are boring if you eat them every day."
+    }, 
+    {
+        question: "When one professor lectures many students, some will inevitably have trouble keeping up, while others will get too far ahead in their studies. I wonder how this problem might be solved…", 
+        answer: "Perhaps we should make lectures optional"
+    }, 
+    {
+        question: "Such interesting ideas you seem to have, Professor, but tell me this: what would you do if you were given the task of teaching a subject than you yourself did not understand?", 
+        answer: "I’d take the time to study the subject before teaching it."
+    }, 
+    {
+        question: "Pretty soon I’ll have eaten everything they have to offer in the dining hall. Then what am I supposed to do? Keep eating the same boring meals over and over again?", 
+        answer: "You should just enjoy` what you’ve got"
+    }, 
+    {
+        question: "Um. Sorry, but you should probably just skip me. I don’t have a question to ask. How would I even think of one?", 
+        answer: "You just did."
+    }, 
+    {
+        question: "I am an avid arms collector. Since coming to live here, however, I have had much more trouble finding new items for my collection. Any ideas as to where I might find some uncommon weapons?", 
+        answer: "Ask a blacksmith to teach you how to craft your own."
+    }, 
+    {
+        question: "When I take a lecture I’m not interested in, I get so...sleepy. I just can't fight it. I should just give up, don’t you think? Close my eyes and enjoy a peaceful slumber?", 
+        answer: "No need to fight it. You can’t help it."
+    }, 
+    {
+        question: "I am having a question. I wish for improvement of my lance skills. But I am having uncertainty about how to be training effectively. What should I be doing?", 
+        answer: "Try studying the movements of others for improvement."
+    }, 
+    {
+        question: "Lectures about strategy or reason require so much focus, I end up absolutely famished! But if I go to class after a big meal, I can’t help getting sleepy…", 
+        answer: "Bring along a snack, just in case."
+    }, 
+    {
+        question: "I’m...kind of terrified to walk the monastery grounds at night. It’s unnerving how dark and quiet the place gets! How can I conquer my fear?", 
+        answer: "Think of it as training for night marches."
+    }, 
+    {
+        question: "I enjoy a good ghost story, but I’ve never seen a ghost in real life. I would love to have that experience for myself, even just once!", 
+        answer: "Maybe you need a ritual to summon one."
+    }, 
+    {
+        question: "Art has never been my forte. Even when I apply myself and sketch a ton, I don’t seem to improve much. I can envision an image so perfectly in head, but turning it into something physical that I feel good about eludes me...", 
+        answer: "Don’t worry about the end result. Just keep practicing."
+    }, 
+    {
+        question: "Magic is not my forte. Being a gifted swordsman, I think my time is best served practicing swordplay technique. Why should I waste my time attending lectures, just to improve my magic skills from mediocre to average?", 
+        answer: "Knowing magic will make you a stronger swordsman."
+    },
+    {
+        question: 'Though I have served His Highness for four years, he has eluded my every attempt to learn his favorite foods.',
+        answer: 'Maybe he just likes everything.'
+    },
+    {
+        question: 'I’ve been trying to win the heart of this girl, but none of the fancy gifts or weet things I tell her have worked. Do you know any secret dating tricks I can use to win her heart?',
+        answer: 'Stay away for a while so she starts to miss you.'
+    },
+    {
+        question: 'No matter the weapon, it cannot be wielded properly without ample strength and stamina, regardless of the skills one may possess. I’ve been using various training techniques, but lately I feel like they all are lacking…',
+        answer: 'Try focusing on one skill at a time, at least for a while.'
+    },
+    {
+        question: 'That was a fascinating lecture, but now my brain is fried. I feel like I need a change of pace to reinvigorate me. What do you recommend?',
+        answer: 'Try going for a stroll around the monastery.'
+    },
+    {
+        question: "There’s so much to do and never enough time in which to do it. Sleep is a nuisance, but my body forces me to slow down, even though my mind doesn’t want to. Do you have any tips for shaking tired feelings away?",
+        answer: "Sleeping when you’re tired is best for you."
+    },
+    {
+        question: "The ladies have been giving me the cold shoulder, of late, I believe it is because they are intimidated by my noble presence. How can I be more...accessible?",
+        answer: "Sounds like it’s their problem, not yours."
+    },
+    {
+        question: "I just wanna eat meat and fish, but everyone in the dining hall says I gotta eat vegetables too! I’m never gonna get full on stupid leaves!",
+        answer: "Eating vegetable will help your muscles grow."
+    },
+    {
+        question: "I don’t care about being pretty or anything, but it bothers me that some people don’t even realize I’m a girl.",
+        answer: "They’re wrong, pay them no mind"
+    },
+    {
+        question: "Um, excuse me… Can you help me? I always get the feeling that I’m being watched. I get so nervous that I can’t sleep at night…",
+        answer: "You might be right. You should be careful."
+    },
+    {
+        question: "I hate cleaning my room. It’s so boring, and to be honest, I’m kinda lazy. But I don’t want my room to get messy. Who do you think I should get to tidy up for me?",
+        answer: "Ask somebody who’s good at cleaning."
+    },
+    {
+        question: "I’m not very good at sharing my opinion. Usually, I just go along with what everybody else thinks. I don’t want to bother anyone. Um..",
+        answer: "They’ll appreciate your opinion. Let yourself be heard."
+    },
+    {
+        question: "When I ask them to take part in my experiments, even the most industrious students run away from me. No matter how sincerely I plead! Do you have any suggestions?",
+        answer: "Make the 'Experiment a 'job' and pay them handsomely."
+    }, 
+    {
+        question: "Some people hate spiders or centipedes, but they have no strong feelings towards flies or crickets. Why is that?",
+        answer: "They may all be bugs, but they’re such different species."
+    },
+    {
+        question: "Hey. Maybe you can answer a question I’ve had for a while now… Why is it that men don’t like women who are messy?",
+        answer: "Men have unrealistic expectations"
+    },
+    {
+        question: "As a member of the knights, I sometimes travel by ship for my missions. But I have a bit of a weak stomach...What can I do to tamp down my sea sickness?",
+        answer: "Medicine and magic should help"
+    },
+    {
+        question: "I want to be able to talk to males and make friends with them, but my brother always causes such a fuss when I do. Do you have any advice for me?",
+        answer: "Do it when he’s not looking."
+    },
+    {
+        question: "One of your students keeps challenging me to fight him, but he has no idea how to wield a sword. It's a waste of time for both of us. What am I supposed to do?",
+        answer: "Drop a hint that he needs a new training partner."
+    },
+    {
+        question: "I'm real happy here, but there's somebody who has up and decided to take pity on me. Without asking! It's real darn irritating, and I want them to know it. I just don't know how I can make myself understood.",
+        answer: "Show them how happy you are working here."
+    },
+    {
+        question: "I heard rumors about ghosts roaming the monastery…. I’m certainly not scared, nor do I have time to worry about it, but...uh… You should probably do something about it, for the sake of the others!",
+        answer: "Let’s bring more candles to the halls to brighten things up."
+    },
+    {
+        question: "People call Captain Jeralt the Blade Breaker. I figure, if I want to be a top-tier mercenary like him, I’m going to need a tough-sounding title of my own. Help me out?",
+        answer: "The Executioner"
+    },
+    {
+        question: "I prefer to use stealth tactics to assassinate my foes on the battlefield, but you take a different approach. Why is that?",
+        answer: "Those tactics don’t give the enemy a chance to surrender."
+    },
+    {
+        question: "The weak-willed care little for how they are governed, but lady Edelgard wishes to open their eyes. How would you achieve this, in her position?",
+        answer: "The only way to help them is to educate them. "
+    },
+    {
+        question: "As I am fighting more, my arrows are decreasing in quality. They are not flying as far and have less accuracy. What can I be doing differently?",
+        answer: "New bowstrings could help you shoot with more force."
+    },
+    {
+        question: "I sometimes find books written in languages I’ve never seen before. I’d love to read them somehow, but I don’t even know where to start… Do you have any ideas?",
+        answer: "Piece things together using linguistic clues."
+    },
+    {
+        question: "I want to knit an animal doll for someone, but I’m not sure what kind to make. They love eagles, lions, and deer equally! I don’t know what to do…",
+        answer: "Why not make one of each?"
+    },
+    {
+        question: "Hey, Professor! I think I’m ready… I think it’s time you finally gave me a nickname! What’s a guy gotta do to earn one?",
+        answer: "You actually have one, but it’s too scary to say out loud. "
+    },
+    {
+        question: "The anniversary of Jeralt’s death is coming up. I’d like to put something on his grave, so that he knows we’re all thinking of him. What item would make him happiest?",
+        answer: "The best bottle of booze you can find."
+    },
+    {
+        question: "I’ve had this sword for a long time. It is as much a part of me as the hand that holds it. But a good swordsmith could craft a weapon that’s more effective and durable. Should I stick with my old, familiar sword, or acquire a better one?",
+        answer: "Get the new weapon. Give yourself time to train with it."
+    },
+    {
+        question: "Death is an inevitable result of military conflict. Not only soldiers, but also warhorses, who have no say in their line of work, lay down their lives on the battlefield. What steps can a commander take to minimize this loss of life?",
+        answer: "Adopt new tactics that increase survivability."
+    },
+    {
+        question: "I know this is highly unlikely, but on the off chance that Rhea surrenders to my terms, what should I do? I would appreciate your thoughts on the matter…",
+        answer: "Strip her of her power so she can’t interfere in politics."
+    },
+    {
+        question: "All this savagery, day after day, is depressing. Do you have any ideas for fun things I could do to feel better, Professor?",
+        answer: "Why don’t you ask a girl out?"
+    },
+    {
+        question: "Some of the children were admiring a painting of mine. I’d like to paint a new piece. Something just for them, to make them happy. What do you think I ought to paint?",
+        answer: "Woodland creatures."
+    },
+    {
+        question: "I haven’t been able to see my brother and sister lately, so I’m sending them gifts instead. My brother’s not one to ask for anything, though. I don’t know what to give him?",
+        answer: "Maybe you should send him some books."
+    },
+    {
+        question: "Songs are so powerless, Professor. No matter how much on sings, there’s no soldier on the battlefield who will listen. It’s horrible that one can only sing of peace in times of peace.",
+        answer: "If we can achieve peace, those songs won’t be powerless."
+    },
+    {
+        question: "I think alcohol consumption increases as we continue to fight, day after day after day after… Oh, I mean for the monastery. As a whole, dear. Not just for me. Still, is it something we should be careful about?",
+        answer: "Focus on your own intake. I'll worry about everyone else."
+    },
+    {
+        question: "When I do nothing but fight, my spirits falter and I lose the will to keep things tidy. This is when my room falls to disorder, and my spirits falter even more Is there no way to break this cycle?",
+        answer: "Take a short break to do something that raises your spirits."
+    },
+    {
+        question: "Fishing is what truly calms me, but it is time-consuming. What do you think should I do when a visit to the pond is too inconvenient?",
+        answer: "Write a story about someone fishing"
+    },
+    {
+        question: "I don’t think there’s much difference between Almyra and Fodlan. But everyone else seems to think Almyra is so completely different. Why’s that.",
+        answer: "It’s prejudice towards unknown places, nothing more."
+    },
+    {
+        question: "I have a tactical question, Professor. Let’s say you’re leading an army and the enemy, whose forces greatly outnumber your own, has you surrounded. What would you do in that situation?",
+        answer: "Push forward. Punch a hold in their formation."
+    },
+    {
+        question: "Just thinking about how, even now, someone out there is fighting for something, I just can’t seem to sit still, even during my breaks. What should I do at times like that?",
+        answer: "You could assist with my daily tasks, if you’d like"
+    },
+    {
+        question: "Seeing the cathedral in such a run-down state makes me feel so sorry for the goddess… As soon as this way is finally over, I’m going to devote myself to making this place more beautiful than ever!",
+        answer: "Let’s plant flowers to make it more colorful"
+    },
+    {
+        question: "Soon a day will come when we must all part ways. I get so sad thinking about it.... It's not practical to keep focusing on this though, is it?",
+        answer: "The goddess will always protect everyone."
+    },
+    {
+        question: "Those from northern Faerghus are used to the frigid cold. Although Garreg Mach is located atop a mountain, the summer heat can be quite unbearable for soldiers from the north. What can I do to boost their morale?",
+        answer: "Cooling off in a nearby river is always a good option."
+    },
+    {
+        question: "Whenever I visit home, my brother yammers on and on about all the things I'm doing wrong. It's SO exhausting, I wish he'd back off and let me live my life. What do you think I should do?",
+        answer: "Yammer back at him! See how he likes it!"
+    },
+    {
+        question: "I had to leave my grandpa in charge of taking care of my little sis while I'm away. I was thinking I should send him something nice to say thanks. Any suggestions?",
+        answer: "A tough-looking hat."
+    },
+    {
+        question: "Times are hard... My favorite food cart that sold the most amazing grilled delights went under. I don't even know what to live for anymore...",
+        answer: "Maybe the cart will open back up if the war ends."
+    },
+    {
+        question: "Considering the current situation and the forces at our command, we will need to rely on mercenaries, However, they have no loyalty to territory or master. I cannot help but feel ill-at-ease about hiring sell swords.",
+        answer: "Mercenary or not, some people will betray your trust"
+    },
+    {
+        question: "I have always been confident in my exceptional skill, but... lately I will admit I have experienced some minor doubts. What can I do to restore my confidence?",
+        answer: "Remember all you've done. Trust that you've been useful."
+    },
+    {
+        question: "I desperately want to travel to some of the places my mother and I spent time in, but I'm not really allowed to leave the monastery walls for such things. It makes me feel so sad... Is there anything I can do?",
+        answer: "Close your eyes and pretend she is with you."
+    }
+]; 
+
+exports.lectureAnswers = lectureAnswers; 
