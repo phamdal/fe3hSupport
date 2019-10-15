660ddd47dff80842e427c3e371d762e3ac7bac5a
Dalena <dalenapham35@gmail.com>
2019-10-14 22:42:43 -0700

    Fix Heroku Port Issue

--- server.js
+++ server.js
@@ -234,7 +234,7 @@ app.get('/sylvain', function(req, res)
     res.send(constants.Sylvain); 
 }); 
 
-var server = app.listen(3000, function()
+var server = app.listen(process.env.PORT || 3000, function()
 {
 
 }); 
